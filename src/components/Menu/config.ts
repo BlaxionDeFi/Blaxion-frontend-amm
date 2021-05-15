d523037d50b3169b6f62e033b601001f8143100a
BourbonDefi <83989772+BlaxionDeFi@users.noreply.github.com>
2021-05-14 21:22:54 -0400

    add discord

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -7,14 +7,9 @@ const config: MenuEntry[] = [
     href: 'https://www.aztecswap.com/',
   },
   {
-    label: 'Bourbon DeFi',
-    icon: 'HomeIcon',
-    href: 'https://www.bourbondefi.com/'
-  },
-  {
     label: 'Trade',
     icon: 'TradeIcon',
-    initialOpenState: true,
+    initialOpenState: false,
     items: [
       {
         label: 'Exchange',
@@ -37,25 +32,16 @@ const config: MenuEntry[] = [
     href: 'https://www.aztecswap.com/pools',
   },
   {
-    label: 'Moctezuma Sacrifice',
-    icon: 'TicketIcon',
-    href: '/lottery',
-  },
-  {
-    label: 'Aztec Gold Token',
+    label: 'Blaxion Token',
     icon: 'InfoIcon',
     items: [
       {
         label: 'Dex Guru',
-        href: 'https://dex.guru/token/0x8298fea38aa8627316ffd943fe001b69cac8174d-bsc',
+        href: 'https://dex.guru/token/0x33e020D046Cd410d12021DFaf01B3a0e483FfA02-bsc',
       },
       {
         label: 'Contract',
-        href: 'https://bscscan.com/token/0x8298fea38aa8627316ffd943fe001b69cac8174d#readContract',
-      },
-      {
-        label: 'AstroTools',
-        href: 'https://app.astrotools.io/pancake-pair-explorer/0xa040492a50f0034894eb6df0a4d6c8ac33b92cfd',
+        href: 'https://bscscan.com/token/0x33e020D046Cd410d12021DFaf01B3a0e483FfA02#readContract',
       },
     ],
   },
