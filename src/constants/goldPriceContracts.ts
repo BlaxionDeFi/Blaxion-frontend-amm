d7f1555ece48e6ea9e69f80b69db2e49cedc7199
BourbonDefi <83989772+BlaxionDeFi@users.noreply.github.com>
2021-05-15 10:15:00 -0400

    Update goldPriceContracts.ts

--- src/constants/goldPriceContracts.ts
+++ src/constants/goldPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0xF1F216B1b59EadAa4b6985F0574fF21383830634',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0xb50539EAC8971052a96A2E466C6865b942835C56'
+  lpAddress: '0xB87F0EA454e198838B06EA04077f97590C27e15B'
 }
 
 export default priceContracts
